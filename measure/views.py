6a1a1ca7c01141f1ce4063d257d8fb9f9c2e7a06
alejocram <jcarmon4@eafit.edu.co>
2020-03-02 15:14:45 -0500

    Se crea el ViewSet como clase del modelo.

--- measure/views.py
+++ measure/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Measure
+from .serializers import MeasureSerializer
 
-# Create your views here.
+class MeasureViewSet(viewsets.ModelViewSet):
+    queryset = Measure.objects.all().order_by('-created')
+    serializer_class = MeasureSerializer
